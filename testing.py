4f17c7981aeef9c7844530231a436bd74d8122a5
meharunniza1234 <mn9937112@gmail.com>
2025-08-22 19:03:54 +0530

    first commit

---
+++ testing.py
@@ -0,0 +1,19 @@
+def display():
+    x=10
+    y=3
+    print(x/y)
+    return x/y
+
+a=int(5.2)
+b=4.3
+c=a+b
+print(c)
+breakpoint()
+
+d=7
+e=2
+print(d+e)
+breakpoint()
+
+result=display()
+print("finished")
